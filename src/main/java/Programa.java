eac6dcc1c2a864bdd7d42005c9726779f086f323
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2021-09-28 14:24:02 -0500

    Clase con método mein agregada

---
+++ src/main/java/Programa.java
@@ -0,0 +1,8 @@
+import vista.GUI_EstudianteCRUD;
+
+public class Programa {
+    public static void main(String[] args) {
+        GUI_EstudianteCRUD ventana = new GUI_EstudianteCRUD();
+        ventana.setVisible(true);
+    }
+}
